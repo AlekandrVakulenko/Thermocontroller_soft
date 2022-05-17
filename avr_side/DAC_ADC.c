ae49b81bf47f6728e6a0c5fa920daa2dd8aaad01
Aleksandr Vakulenko <sasha705@mail.ru>
2022-05-17 14:21:23 +0300

    SPI update

--- avr_side/DAC_ADC.c
+++ avr_side/DAC_ADC.c
@@ -10,38 +10,43 @@ union {
 } spi_buffer;
 
 
+uint8_t SPIpush(uint8_t arg){
+	SPDR1 = arg;
+	while (!(SPSR1 & 0b10000000)){_NOP;}
+	arg = SPDR1;
+	return arg;
+}
+
+
 void DAC_set(uint16_t arg){
+	SPCR1 = 0b01010101; //FOR DAC
+	
 	if (arg >= 4096){
 		arg = 4095;
 	}
 	spi_buffer.DWORD = (uint32_t)(arg << 2);
-	
-	SPCR1 = 0b01010101; //FOR DAC
-	_NOP;
+
 	cli();
 	DAC_CSClear;
 	for (int8_t i=1; i>=0; --i){
-		SPDR1 = spi_buffer.CHAR[i];
-		while (!(SPSR1 & 0b10000000)){_NOP;}
-		spi_buffer.CHAR[i] = SPDR1;
+		spi_buffer.CHAR[i] = SPIpush(spi_buffer.CHAR[i]);
 	}
 	DAC_CSSet;
 	sei();
 }
 
 uint16_t ADC_read_low_level(void){
-	//SPCR1 = 0b01010101; //SPI init for DAC
 	SPCR1 = 0b01010001; //FOR ADC
 	_NOP;
+	
 	cli();
 	ADC_CSClear;
 	for (int8_t i=2; i>=0; --i){
-		SPDR1 = spi_buffer.CHAR[i];
-		while (!(SPSR1 & 0b10000000)){_NOP;}
-		spi_buffer.CHAR[i] = SPDR1;
+		spi_buffer.CHAR[i] = SPIpush(spi_buffer.CHAR[i]);
 	}
 	ADC_CSSet;
 	sei();
+	
 	return (uint16_t)((spi_buffer.DWORD >> 4) & 0x0000FFFF);
 }
 
